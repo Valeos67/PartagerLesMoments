b60cf891067d1a5187b7d4180c03c66866eceeb1
Valentin SCHERER <valentinscherer@MacBook-Pro-de-Valentin.local>
2018-09-10 18:49:03 +0200

    Preparation UI

---
+++ Partager les moments/PhotoController.swift
@@ -0,0 +1,33 @@
+//
+//  PhotoController.swift
+//  Partager les moments
+//
+//  Created by Valentin SCHERER on 10/09/2018.
+//  Copyright © 2018 Valentin SCHERER. All rights reserved.
+//
+
+import UIKit
+
+class PhotoController: UIViewController {
+
+    @IBOutlet weak var partagerBouton: UIBarButtonItem!
+    @IBOutlet weak var photoAPartager: UIImageView!
+    @IBOutlet weak var texteAPartager: UITextView!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func boutonPartagerAppuyer(_ sender: Any) {
+    }
+    
+    
+    
+    
+}
